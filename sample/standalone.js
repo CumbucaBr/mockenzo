a090303a493e0de865abc260c57733bd5e1c1825
Charleston Anjos <charleston.anjos@gmail.com>
2022-07-08 13:08:20 -0300

    add socket example

--- sample/standalone.js
+++ sample/standalone.js
@@ -1,26 +1,49 @@
 const Mockenzo = require("../index")
 
-Mockenzo()
-    .on(3000)
-    .post('/enzo')
-        .delay(3000)
-        .statusCode(400)
-        .response({
-            'error_code': 400,
-            'error_description': 'Bad Request'
-        })
+const mockJsonFile = require('./mocks/list.json')
+
+const httpMockenzo = Mockenzo()
+    .http()//init config http
+        .on(3000)//listen on port
+            .post('/enzo')//init config route
+            .delay(3000)//add delay for response
+            .statusCode(400)//send status code
+            .response({
+                'error_code': 400,
+                'error_description': 'Bad Request'
+            })
+            .run()//finish config and run
+        .get('/enzo')//init new route
+            .statusCode(401)
+            .response({
+                'error_code': 401,
+                'error_description': 'Unauthorized'
+            })
         .run()
-    .get('/enzo')
-        .statusCode(401)
-        .response({
-            'error_code': 401,
-            'error_description': 'Unauthorized'
-        })
-        .run()
-    .get('/enzos')
-        .statusCode(200)
-        .responseJsonFile('../sample/mocks/list.json')
+            .get('/enzos')
+            .statusCode(200)
+            .responseJsonFile(mockJsonFile)
         .run()
     .start(() => {
-        console.log("server is running")
+        console.log("http server is running")
     });
+
+const socketMockenzo = Mockenzo()
+    .socket()
+        .on('/', 3001)
+            .onConnection(() => {
+                console.log('received new connection')
+            })
+            .onMessage((data) => {
+                console.log(data)//print message sent to server
+                socketMockenzo.send("successful")//clients receive "successful"
+            })
+            .onError((error) => {
+                console.error('received error', error)
+            })
+            .onClose(() => {
+                console.log("closing socket server")
+            })
+        .start(() => {
+            console.log("socket server is running")
+        })
